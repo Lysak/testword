1e270aa126e46bfa11922dcf5b3fbeb5d09b0f95
Dmytrii Lysak <dlysak@live.com>
2017-09-11 09:27:52 +0300

    add ckeditor

--- views/admin/create.php
+++ views/admin/create.php
@@ -10,10 +10,12 @@
             <label for="pwd">Короткий опис:</label>
             <textarea class="form-control" name="short_content" rows="5" id="comment" required></textarea>
         </div>
+        <script src="//cdn.ckeditor.com/4.7.2/standard/ckeditor.js"></script>
         <div class="form-group">
             <label for="comment">Детально:</label>
             <textarea class="form-control" name="content" rows="8" id="comment" required></textarea>
         </div>
+
         <div class="form-group">
             <label for="comment">Додати зображення:</label>
             <input type="file" name="image" class="file">
@@ -21,5 +23,8 @@
 
         <button class="btn btn-lg btn-primary btn-block" name="submit" type="submit">ДОДАТИ НОВИНУ</button>
     </form>
-
+    </br>
+    <script>
+        CKEDITOR.replace("content");
+    </script>
 <?php include ROOT . '/views/layouts/footer.php'; ?>
